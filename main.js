18f2e29e436350e4845c6df524c57220d9008390
BarabAnna <hanna.barabanova@gmail.com>
2016-02-08 00:34:44 +0300

    add background and first hero

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
  * Created by hannaBar on 13.01.2016.
  */
 //alert('hochu pisat na russkom');
+var result = 0;
+var anulik = 'barsik';
+for (var i = 0; i < 100; i++) {
+    result += i;
+    if (result % 10 === 0)
+        anulik = 'krasotka';
+}
+alert(result);
